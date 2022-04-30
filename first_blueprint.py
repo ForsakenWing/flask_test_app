276b18543726cd8646c3de42b12d3cce436e9dea
Spartak Karapetian <s.karapetian@adtelligent.com>
2022-04-30 20:12:01 +0200

    Endpoints initialized. Preparing app for deploying

--- first_blueprint.py
+++ first_blueprint.py
@@ -1,4 +1,4 @@
-from flask import Blueprint
+from flask import Blueprint, request, abort
 
 v1_bp = Blueprint('v1', __name__, url_prefix='/v1')
 
@@ -13,6 +13,27 @@ def get_request():  # put application's code here
     return f'Hello, you successfully sent the get request!'
 
 
-@v1_bp.route('/post')
+@v1_bp.route('/post', methods=['GET', 'POST'])
 def post_request():
-    return f'Hello, you successfully sent the post request'
+    if request.method == 'POST':
+        return f'Hello, you successfully sent the post request'
+    return abort(405)
+
+
+@v1_bp.route('/patch', methods=['GET', 'PATCH'])
+def patch_request():
+    if request.method == 'PATCH':
+        return f'Hello, you successfully sent the patch request'
+    return abort(405)
+
+
+@v1_bp.route('/put', methods=['GET', 'PUT'])
+def put_request():
+    if request.method == 'PUT':
+        return f'Hello, you successfully sent the put request'
+    return abort(405)
+
+
+@v1_bp.errorhandler(405)
+def method_not_allowed(error):
+    return f"OWN RESPONSE This method: '{request.method}' is not allowed", 405
