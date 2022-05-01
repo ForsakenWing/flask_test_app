1e1cd6c14b2f78610a82f29ac3645dde6a932dab
Spartak Karapetian <s.karapetian@adtelligent.com>
2022-05-01 13:54:38 +0200

    Dockerfile added

---
+++ .flaskenv
@@ -0,0 +1,6 @@
+FLASK_APP=setup:create_app()
+FLASK_ENV=production
+FLASK_RUN_PORT=5000
+FLASK_DEBUG=0
+FLASK_RUN_HOST=0.0.0.0
+
--- setup.py
+++ setup.py
@@ -1,14 +1,20 @@
+import os
+
 from first_blueprint import v1_bp
 from second_blueprint import alive
 from flask import Flask
+from dotenv import load_dotenv
 
 
-def run_app():
+def create_app():
     app = Flask(__name__)
     app.register_blueprint(v1_bp)
     app.register_blueprint(alive)
-    app.run(debug=True)
+    return app
 
 
 if __name__ == '__main__':
-    run_app()
+    load_dotenv('.flaskenv')
+    flask_app = create_app()
+    if os.getenv('FLASK_ENV') != 'development':
+        flask_app.run(use_reloader=False, host='0.0.0.0', port=5000)
